961029370ac1c042fa35fd2b83c6e9d68beaf8d8
Ronni Elken Lindsgaard <ronni.lindsgaard@gmail.com>
2012-08-01 12:41:51 +0000

    Implemented ga_tracking for views

--- app/code/local/TheSocialDigits/Recommendations/Block/Recommendations.php
+++ app/code/local/TheSocialDigits/Recommendations/Block/Recommendations.php
@@ -7,6 +7,10 @@ Mage_Core_Block_Template {
     Mage::getStoreConfig('recommendations_options/settings/api_key');
   }
 
+  public function getTrackingCategory(){
+    return Mage::getStoreConfig('recommendations_options/settings/ga_tracking');
+  }
+
   public function addJs($path){
     $head = $this->getLayout()->getBlock('head');
     return $head->addJs($path);
