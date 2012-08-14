23b21361d9a3e8d5b9fee7849602f66daa6a485a
Ronni Elken Lindsgaard <ronni.lindsgaard@gmail.com>
2012-08-14 14:24:56 +0000

    Fixed jQuery vs prototype conflict

--- js/thesocialdigits-js/config.thesocialdigits.js
+++ js/thesocialdigits-js/config.thesocialdigits.js
@@ -1,17 +1,18 @@
-(function($){
-  $(document).ready(function(){
-    $.get('http://demoshop.thesocialdigits.com/index.php/recommendations/configuration',
+(function(j){
+  j(document).ready(function(){
+    j.get('http://demoshop.thesocialdigits.com/index.php/recommendations/configuration',
     function(data){
       data = JSON.parse(data);
       //datasource is set in the javascript file because of unknwon error
       data.datasource = function(products,callback){
         var url = '/index.php/recommendations/list';
         var data = {'products': products};
-        $.getJSON(url,data,callback);
+        j.getJSON(url,data,callback);
       }
-      $.thesocialdigits(data);
+      j.thesocialdigits(data);
       //Sends out an event that thesocialdigits' api is now ready
-      $(document).trigger('thesocialdigits.ready');
+      j(document).trigger('thesocialdigits.ready');
     });
   });
 })(jQuery);
+jQuery.noConflict();
