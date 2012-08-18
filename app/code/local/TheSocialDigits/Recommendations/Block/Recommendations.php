bd4ca0479338660a539131279bcfdecf7a1eb356
Ronni Elken Lindsgaard <ronni.lindsgaard@gmail.com>
2012-08-18 13:49:36 +0000

    Made recommendations block more advanced and renamed recommendations.phtml to sidebar.phtml

--- app/code/local/TheSocialDigits/Recommendations/Block/Recommendations.php
+++ app/code/local/TheSocialDigits/Recommendations/Block/Recommendations.php
@@ -2,7 +2,29 @@
 class TheSocialDigits_Recommendations_Block_Recommendations extends
 Mage_Core_Block_Template {
 
-  private $_arguments = array('limit' => 3);
+  private $_api_arguments = array(
+    'products' => array(),
+    'limit' => 3,
+//    'visitor' => null,
+    'exclude' => array(),
+//    'filter' => "",
+  );
+
+  private $_slider_arguments = array(
+    'visible' => 3,
+    'step' => 3,
+    'width' => 100,
+    'height' => 100,
+    'speed' => 1000,
+    'margin' => 0,
+    'orientation' => 'horizontal',
+    'auto_enabled' => TRUE,
+    'auto_direction' => 'next',
+    'auto_interval' => 5000,
+    'startAtPage' => 0,
+    'navigation_next' => false,
+    'navigation_prev' => false,
+  );
 
   public function _prepareLayout(){
     $this->addJs('jquery-1.7.2.min.js');
@@ -36,7 +58,7 @@ Mage_Core_Block_Template {
 
   public function getProductIdJson(){
     $product_id = $this->getProductId();
-    if(!is_null)
+    if(!is_null($product_id))
       return json_encode(array($product_id),JSON_NUMERIC_CHECK);
     return '[]';
   }
@@ -79,30 +101,104 @@ Mage_Core_Block_Template {
     return isset($this->_action) ? $this->_action : 'related';
   }
 
-  public function setHtmlTemplateId($id){
-    $this->_htmlTemplateId = $id;
-  }
-
-  public function getHtmlTemplateId(){
-    return isset($this->_htmlTemplateId) ? $this->_htmlTemplateId :
-    'productsTemplate';
-  }
-
-  public function setArgument($argument,$value){
-    $this->_arguments[$argument] = $value;
+  protected function _validateApiArgument($argument, $value){
     return true;
   }
 
-  public function getArgument($argument){
-    return isset($this->_arguments[$argument]) ?
-    $this->_arguments[$argument] : NULL;
+  public function setApiArgument($argument,$value){
+    if($this->_validateApiArgument($argument,$value)){
+      $this->_api_arguments[$argument] = $value;
+      return true;
+    }
+    return false;
   }
 
-  public function getArguments(){
-    return $this->_arguments;
+  public function getApiArgument($argument,$default_value=NULL){
+    return isset($this->_api_arguments[$argument]) ?
+      $this->_api_arguments[$argument] : $default_value;
   }
 
-  public function getArgumentsJson(){
-    return json_encode($this->getArguments(),JSON_NUMERIC_CHECK);
+  protected function _prepareApiArguments(){
+    $arguments = array();
+    foreach($this->_api_arguments as $arg => $val){
+      switch($arg){
+        case 'products':
+          $arguments['products'] = array($this->getProductId());
+        break;
+        case 'exclude':
+          $arguments['exclude'] = $this->getCartContents();
+        break;
+        default:
+          $arguments[$arg] = $val;
+      }
+    }
+    return $arguments;
+  }
+
+  protected function getApiArguments(){
+    return $this->_prepareApiArguments();
+  }
+
+  public function getApiArgumentsJson(){
+    return json_encode($this->getApiArguments(),JSON_NUMERIC_CHECK);
+  }
+
+  protected function _validateSliderArgument($argument, $value){
+    return true;
+  }
+
+  public function setSliderArgument($argument, $value){
+    if($this->_validateSliderArgument($argument, $value)){
+      $this->_slider_arguments[$argument] = $value;
+      return true;
+    }
+    return false;
+  }
+
+  public function getSliderArgument($argument, $default_value=NULL){
+    return isset($this->_slider_arguments[$argument]) ?
+      $this->_slider_arguments[$argument] : $default_value;
+  }
+
+  protected function _prepareSliderArguments(){
+    $arguments = array();
+    foreach($this->_slider_arguments as $arg => $val){
+      switch($arg){
+        case 'step':
+          if($val > $this->getSliderArgument('visible',$val))
+            $arguments['step'] = $this->getSliderArgument('visible',$val);
+        break;
+        case 'auto_enabled':
+          $arguments['auto']['enabled'] = $val;
+        break;
+        case 'auto_direction':
+          $arguments['auto']['direction'] = $val;
+        break;
+        case 'auto_interval':
+          $arguments['auto']['interval'] = $val;
+        break;
+        case 'navigation_prev':
+          if($val)
+            $arguments['navigation']['prev'] = '#' . $this->getElementId() .
+              '-prev';
+        break;
+        case 'navigation_next':
+          if($val)
+            $arguments['navigation']['next'] = '#' . $this->getElementId() .
+              '-next';
+        break;
+        default:
+          $arguments[$arg] = $val;
+      }
+    }
+    return $arguments;
+  }
+
+  protected function _getSliderArguments(){
+    return $this->_prepareSliderArguments();
+  }
+
+  public function getSliderArgumentsJson(){
+    return json_encode($this->_getSliderArguments(),JSON_NUMERIC_CHECK);
   }
 }
