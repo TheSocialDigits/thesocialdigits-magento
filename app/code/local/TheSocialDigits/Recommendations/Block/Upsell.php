636651fdffb4462ea1dc38cede93b54524737922
Ronni Elken Lindsgaard <ronni.lindsgaard@gmail.com>
2012-08-24 12:39:55 +0000

    Added configurations to upsell and made it even prettier

---
+++ app/code/local/TheSocialDigits/Recommendations/Block/Upsell.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once 'TheSocialDigits/Recommendations/Block/Recommendations.php';
+
+class TheSocialDigits_Recommendations_Block_Upsell extends
+TheSocialDigits_Recommendations_Block_Recommendations {
+
+  public function _construct(){
+    $this->setCarouselArguments(array(
+      'visible' => $this->getStoreConfig('visible',4,'0'),
+      'width' => $this->getStoreConfig('width',115,'0'),
+      'height' => $this->getStoreConfig('height',150,'0'),
+      'speed' => $this->getStoreConfig('transition_speed',2000,'0'),
+    ));
+  }
+
+  public function getType(){
+    return 'upsell';
+  }
+}
