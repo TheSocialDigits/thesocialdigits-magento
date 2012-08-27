bec179c41a9acc97e6032c2a834261f59504ee24
Ronni Elken Lindsgaard <ronni.lindsgaard@gmail.com>
2012-08-27 14:00:24 +0000

    product_url now points to super product

--- app/code/local/TheSocialDigits/Recommendations/controllers/ListController.php
+++ app/code/local/TheSocialDigits/Recommendations/controllers/ListController.php
@@ -9,11 +9,23 @@ Mage_Core_Controller_Front_Action {
       foreach($params['products'] as $item_id){
         $product = Mage::getModel('catalog/product')
           ->load($item_id);
+        if($product->getTypeId() == 'simple') {  
+          $configurable_product_model_obj =
+            Mage::getModel('catalog/product_type_configurable');  
+          $parents =
+            $configurable_product_model_obj->getParentIdsByChild($product->getId() );
+        }
         $item_data = $product->getData();
         $item_data['id'] = $item_data['entity_id'];
         $item_data['thumbnail_url'] = $product->getThumbnailUrl();
-        $item_data['product_url'] = $item_data['url_path'];
+        if(sizeof($parents))
+          $item_data['product_url'] = Mage::getModel('catalog/product')
+          ->load($parents[0])
+          ->getUrlPath();
+        else
+          $item_data['product_url'] = $product->getUrlPath();
         $items[] = $item_data;
+
       }
       echo json_encode($items,JSON_NUMERIC_CHECK);
     } else {
